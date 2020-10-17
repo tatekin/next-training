5a7017da5584b7a0879f2544769c2de3e027d91f
manbow <yuukisasaki42@icloud.com>
2020-10-17 12:50:22 +0900

    edit blog contents

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>マンボウブログ</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Next.jsチュートリアルは
+          <a href="https://nextjs.org/learn" target="_blank" rel="noopener noreferrer">こちら</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
